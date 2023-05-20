e25819ee7583b1deb3f35008c49f9b779e1cb099
AnatolySoroka <toliksoroka66@gmail.com>
2023-05-20 22:36:32 +0300

    created the ukranian heroes section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
     $(".about__slider").slick({
+        arrow: true,
         slidesToShow: 1,
+        centerPadding: '0px'
+    });
+});
+
+
+$(document).ready(function () {
+    $(".ukrainian-heroes__slider").slick({
+        arrow: true,
+        slidesToShow: 3,
         centerPadding: '0px'
     });
 });
