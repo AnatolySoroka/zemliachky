04032de014c88dccaf3ab8aa52adb66384b7e271
AnatolySoroka <toliksoroka66@gmail.com>
2023-05-22 00:08:01 +0300

    created the footer section

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
     $(".about__slider").slick({
         arrow: true,
         slidesToShow: 1,
-        centerPadding: '0px'
+        centerPadding: '0px',
+        autoplay: true,
     });
 });
 
@@ -11,6 +12,19 @@ $(document).ready(function () {
     $(".ukrainian-heroes__slider").slick({
         arrow: true,
         slidesToShow: 3,
-        centerPadding: '0px'
+        // centerPadding: '0px',
+        // centerMode: true,
+        // adaptiveHeight: true,
+        // autoplay: true,
+        // variableWidth: true
+    });
+});
+
+$(document).ready(function () {
+    $(".partners__slider").slick({
+        arrow: true,
+        slidesToShow: 4,
+        // centerPadding: '0px',
+        // autoplay: true,
     });
 });
